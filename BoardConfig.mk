ab2add41294b1cf37464b0da2a59a750a5993a9b
James Nguyen <tienthong2004@gmail.com>
2022-04-29 15:45:59 +0700

    sepolicy: Kang new SEPolicy from LineageOS/android_device_samsung_gts4lv-common
    Signed-off-by: James Nguyen <tienthong2004@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,7 +203,11 @@ BOARD_ROOT_EXTRA_FOLDERS := config omr efs
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp_policy
 
 # SELinux
+include device/qcom/sepolicy/sepolicy.mk
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # WiFi
 BOARD_HAVE_SAMSUNG_WIFI := true
